e08061824bd24cc832edcfda8654bbb58aabb947
Eric Fruelda <ericfruelda@ymail.com>
2016-01-28 16:44:19 +1100

    Updated files.

--- ui.R
+++ ui.R
@@ -11,8 +11,8 @@ shinyUI(fluidPage(
     sidebarPanel(
         helpText("Please enter values within the specified limits. Helpful information is provided in the About tab located in the main page."),
         sliderInput("age", "Age (in years, 1-100):", 30, min = 1, max = 200, step=1),
-        sliderInput("emp", "Years at Current Employer (0-100):", 10, min = 1, max = 100, step=1),
-        sliderInput("add", "Years at Current Address (0-100):", 10, min = 1, max = 100, step=1),
+        sliderInput("emp", "Years at Current Employer (0-100):", 10, min = 0, max = 100, step=1),
+        sliderInput("add", "Years at Current Address (0-100):", 10, min = 0, max = 100, step=1),
         numericInput("cin", "Current Income ($0-$1000000):", 100000, min = 0, max = 1000000, step=1),
         numericInput("ccd", "Current Credit Card Debt ($0-$1000000):", 0, min = 0, max = 1000000, step=1),
         numericInput("cad", "Current Automobile Debt ($0-$1000000):", 0, min = 0, max = 1000000, step=1),
